592d8b9c7f5d9ecb97e38e28214cf3e5980b33b9
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-10-03 10:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const container = document.getElementById("buttons");
+const folder = "./sound";
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const stop = "stop";
+const fragment = document.createDocumentFragment();
+
+sounds.forEach((sound)=>{
+	let button = document.createElement("button");
+	button.className = "btn";
+	button.src = `${folder}${sound}.mp3`;
+	button.textContent = sound;
+	fragment.appendChild(button);
+})
+
+let button = document.createElement("button");
+button.className = "stop";
+button.src = `${folder}${stop}.mp3`;
+button.textContent = stop;
+fragment.appendChild(button);
+
+
+container.appendChild(fragment);
